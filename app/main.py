f7c3ed28173a8386ae69ab9052c6152c6ed93c8a
Arthur Oleinikov <arch0998@gmail.com>
2025-07-06 17:55:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,35 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.lenght = 0
+        self.hash_table = [[] for _ in range(8)]
+
+    def get_hash(self, key: Any) -> int:
+        return hash(key) % len(self.hash_table)
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        index = self.get_hash(key)
+        table = self.hash_table[index]
+
+        for item in table:
+            if item["key"] == key:
+                item["value"] = value
+                return
+
+        table.append({"key": key, "value": value})
+        self.lenght += 1
+
+    def __getitem__(self, key: Any) -> Any:
+        index = self.get_hash(key)
+        table = self.hash_table[index]
+
+        for item in table:
+            if item["key"] == key:
+                return item["value"]
+
+        raise KeyError(f"Key '{key}' not found")
+
+    def __len__(self) -> int:
+        return self.lenght
